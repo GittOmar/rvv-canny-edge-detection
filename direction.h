7dddf3a0591343f2dc11763907f51a2513cf70a1
omarkhaled-ok06 <omarkhaledmomo@gmail.com>
2026-06-18 18:05:45 +0300

    Delete direction.h

--- direction.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "image_io.h"
-#include "sobel.h"
-
-// Returns a buffer of direction values (0,1,2,3) for each pixel
-// 0=0deg, 1=45deg, 2=90deg, 3=135deg
-// Caller must free() the returned buffer
-uint8_t* gradient_direction(const GradientImage& g);
